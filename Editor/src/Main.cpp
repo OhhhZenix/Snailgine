e23885cbdeb802b5c29b4eb1d5522199c4f82564
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2021-03-18 18:49:35 -0600

    Update Main.cpp

--- Editor/src/Main.cpp
+++ Editor/src/Main.cpp
@@ -1,16 +1,1 @@
 #include <Snailgine.hpp>
-
-class App : public sn::Application {
-public:
-	App() {
-
-	}
-
-	~App() override {
-
-	}
-};
-
-sn::Application* sn::create_application() {
-	return new App();
-}
